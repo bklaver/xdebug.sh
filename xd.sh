9ccc85416819e145b997a492456ccaa1fffbd256
Brad Klaver <bklaver@echidna.ca>
2016-05-17 02:02:54 -0400

    Initial Commit, would like to add more variables for general use

---
+++ xd.sh
@@ -0,0 +1,26 @@
+
+
+phpinipath=/c/wamp/bin/apache/apache2.4.9/bin/php.ini
+
+case $1 in
+  on)
+    sed -i 's/xdebug.remote_enable=0/xdebug.remote_enable=1/g' ${phpinipath}
+    sed -i 's%;zend_extension="c:/wamp/bin/php/php5.5.12/zend_ext/php_xdebug-2.2.5-5.5-vc11-x86_64.dll"%zend_extension="c:/wamp/bin/php/php5.5.12/zend_ext/php_xdebug-2.2.5-5.5-vc11-x86_64.dll"%g' ${phpinipath}   
+      echo "XDebug On"
+      ap.sh
+
+  ;;
+  off)
+    sed -i 's/xdebug.remote_enable=1/xdebug.remote_enable=0/g' ${phpinipath}
+    sed -i 's%zend_extension="c:/wamp/bin/php/php5.5.12/zend_ext/php_xdebug-2.2.5-5.5-vc11-x86_64.dll"%;zend_extension="c:/wamp/bin/php/php5.5.12/zend_ext/php_xdebug-2.2.5-5.5-vc11-x86_64.dll"%g'  ${phpinipath}
+    echo "XDebug Off"
+    ap.sh
+  ;;
+  status)
+    grep "zend_ext" ${phpinipath}
+    grep "xdebug.remote_enable" ${phpinipath}
+  ;;
+  *)
+    echo "Usage: php_debug on|off"
+  ;;
+esac
